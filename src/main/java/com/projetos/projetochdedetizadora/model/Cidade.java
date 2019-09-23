acd927c57490a72a992d7f9a2fbbb098d83d43bf
Silvia <silvia.vs.andre@gmail.com>
2019-09-23 13:51:11 -0300

    Revert "Revert "Revert "mapeando as classes"""
    This reverts commit 1cd3c4a42d0338e7140133951849c03a77c138d6.

--- src/main/java/com/projetos/projetochdedetizadora/model/Cidade.java
+++ src/main/java/com/projetos/projetochdedetizadora/model/Cidade.java
@@ -1,34 +1,11 @@
 
 package com.projetos.projetochdedetizadora.model;
 
-import java.io.Serializable;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name="cidade")
-
-public class Cidade implements Serializable  {
-   
-    @Id //chave primaria
-    @GeneratedValue(strategy =GenerationType.IDENTITY)//auto icremente
-    @Column(name="id", nullable = false)//coluna no banco de dados
+public class Cidade {
     private Long id; //NÃO pode ser nulo
-   
-    @Column(name="descricao",length = 100, nullable = false)
     private String descricao; //NÃO pode ser nulo
-   
-    @Column(name="uf",length = 2, nullable = false)
     private String uf; //NÃO pode ser nulo
-     
-    @Column(name="cep",length = 8, nullable = false)
     private Long cep; //NÃO pode ser nulo
-     
-    @Column(name="status",length = 100, nullable = false)
     private String status; //NÃO pode ser nulo
 
     public Long getId() {
